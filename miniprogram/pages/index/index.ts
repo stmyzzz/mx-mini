fac7571836d17f8f7e0ba9baca028029c7755507
stmy <stmyzzz@outlook.com>
2022-07-02 22:27:57 +0800

    feat:swiper done

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,4 +1,7 @@
 // index.ts
+
+import swiper from "../../api/swiper"
+
 // 获取应用实例
 const app = getApp<IAppOption>()
 
@@ -9,28 +12,7 @@ interface swiperList {
 }
 Page({
   data: {
-    swiperList:<swiperList[]>[
-      {
-        imageUrl:"https://www.mclarencars.cn/content/dam/mclaren-automotive/models/720s/720s-spider/sustainment-2021/images/720S_Spider_Front34_Static.jpg",
-        type:'url',
-        target:"https://www.mclarencars.cn/cn-zh/super-series/720s-spider"
-      },
-      {
-        imageUrl:"https://www.mclarencars.cn/content/dam/mclaren-automotive/models/720s/720s/sustainment-2021/images/360_revised.png",
-        type:'url',
-        target:"https://www.mclarencars.cn/cn-zh/super-series/720s"
-      },
-      {
-        imageUrl:"https://www.mclarencars.cn/content/dam/mclaren-automotive/models/720s/720s-spider/sustainment-2021/images/720S_Spider_Front34_Static.jpg",
-        type:'url',
-        target:"https://www.mclarencars.cn/cn-zh/super-series/720s-spider"
-      },
-      {
-        imageUrl:"https://www.mclarencars.cn/content/dam/mclaren-automotive/models/720s/720s/sustainment-2021/images/360_revised.png",
-        type:'url',
-        target:"https://www.mclarencars.cn/cn-zh/super-series/720s-spider"
-      }
-    ],
+    swiperList:<swiperList[]>[],
     currentIndex:0
   },
   onSwiperChange(e:any){
@@ -38,23 +20,33 @@ Page({
       currentIndex:e.detail.current
     })
   },  
-  // 事件处理函数
-  bindViewTap() {
-    
+  handleSwiperItem(e:any){
+     const {item} = e.currentTarget.dataset
+    item.type === 'url' ? wx.navigateTo({
+      url:`/pages/web-view/index?url=${item.target}`
+    }) :wx.navigateTo({
+      url:`/pages/product/detail?id=${item.target}`
+    })
   },
   onLoad() {
-  },
-  getUserProfile() {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
+    swiper.list().then((res:any)=>{
+      console.log('res',res);
+      this.setData({
+        swiperList:res.data
+      })
     })
   }
+  // getUserProfile() {
+  //   // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+  //   wx.getUserProfile({
+  //     desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+  //     success: (res) => {
+  //       console.log(res)
+  //       this.setData({
+  //         userInfo: res.userInfo,
+  //         hasUserInfo: true
+  //       })
+  //     }
+  //   })
+  // }
 })
